0f539fabf5dbc5dc9553a4b1dc0b067116438c8f
Lecter <uxfion@gmail.com>
2024-05-05 12:55:35 +0800

    wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,28 +1,32 @@
 local wezterm = require 'wezterm'
 
-local config = {
-  initial_cols = 110,
-  initial_rows = 40,
-  font_size = 12,
-  font = wezterm.font(
-    "JetBrainsMono Nerd Font",
-    { weight = "Regular" }
-  ),
-  color_scheme = "Dracula",
-  use_fancy_tab_bar = true,
-  hide_tab_bar_if_only_one_tab = true,
-  -- window_decorations = "NONE",
-  show_new_tab_button_in_tab_bar = false,
-  window_background_opacity = 0.9,
-  macos_window_background_blur = 70,
-  enable_kitty_graphics = true,
-}
+local c = {}
+if wezterm.config_builder then
+    c = wezterm.config_builder()
+  end
 
+-- 初始大小
+c.initial_cols = 110
+c.initial_rows = 40
 
-config.default_prog = { 'C:/Users/Lecter/AppData/Local/Microsoft/WindowsApps/Microsoft.PowerShell_8wekyb3d8bbwe/pwsh.exe' }
+-- 关闭时不进行确认
+c.window_close_confirmation = 'NeverPrompt'
+
+c.font_size = 12
+c.font = wezterm.font("JetBrainsMono Nerd Font")
+
+c.color_scheme = "Dracula"
+
+-- 透明背景
+c.window_background_opacity = 0.9
+c.macos_window_background_blur = 70
+
+c.use_fancy_tab_bar = true
+
+c.default_prog = { 'C:/Users/Lecter/AppData/Local/Microsoft/WindowsApps/Microsoft.PowerShell_8wekyb3d8bbwe/pwsh.exe' }
 
 -- -- 启动菜单的一些启动项
--- config.launch_menu = {
+-- c.launch_menu = {
 --   { label = 'MINGW64 / MSYS2', args = { 'C:/msys64/msys2_shell.cmd', '-defterm', '-here', '-no-start', '-shell', 'zsh', '-mingw64' }, },
 --   { label = 'MSYS / MSYS2',    args = { 'C:/msys64/msys2_shell.cmd', '-defterm', '-here', '-no-start', '-shell', 'zsh', '-msys' }, },
 --   { label = 'PowerShell',      args = { 'C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe' }, },
@@ -31,4 +35,4 @@ config.default_prog = { 'C:/Users/Lecter/AppData/Local/Microsoft/WindowsApps/Mic
 -- }
 
 
-return config
+return c
