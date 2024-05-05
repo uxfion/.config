13e88c1bde5a2f30ed3aa92f017bf2387bdfcd8c
Lecter <uxfion@gmail.com>
2024-05-06 00:59:17 +0800

    wezterm win config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,8 +6,8 @@ if wezterm.config_builder then
   end
 
 -- 初始大小
-c.initial_cols = 110
-c.initial_rows = 40
+c.initial_cols = 100
+c.initial_rows = 30
 
 -- 关闭时不进行确认
 c.window_close_confirmation = 'NeverPrompt'
@@ -18,21 +18,24 @@ c.font = wezterm.font("JetBrainsMono Nerd Font")
 c.color_scheme = "Dracula"
 
 -- 透明背景
-c.window_background_opacity = 0.9
-c.macos_window_background_blur = 70
+c.window_background_opacity = 0.95
 
 c.use_fancy_tab_bar = true
 
-c.default_prog = { 'C:/Users/Lecter/AppData/Local/Microsoft/WindowsApps/Microsoft.PowerShell_8wekyb3d8bbwe/pwsh.exe' }
 
--- -- 启动菜单的一些启动项
--- c.launch_menu = {
+c.default_prog = { 'pwsh' }
+c.default_cwd = "D:\\Lecter"
+-- c.default_domain = 'WSL:Ubuntu'
+
+-- 启动菜单的一些启动项
+c.launch_menu = {
+  { label = 'PowerShell', args = { 'pwsh' }, },
+  { label = 'Ubuntu', args = { 'ubuntu' }, },
+
+--   { label = 'nas / ssh',       args = { 'C:/msys64/usr/bin/ssh.exe', 'nas' }, },
 --   { label = 'MINGW64 / MSYS2', args = { 'C:/msys64/msys2_shell.cmd', '-defterm', '-here', '-no-start', '-shell', 'zsh', '-mingw64' }, },
 --   { label = 'MSYS / MSYS2',    args = { 'C:/msys64/msys2_shell.cmd', '-defterm', '-here', '-no-start', '-shell', 'zsh', '-msys' }, },
---   { label = 'PowerShell',      args = { 'C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe' }, },
---   { label = 'CMD',             args = { 'cmd.exe' }, },
---   { label = 'nas / ssh',       args = { 'C:/msys64/usr/bin/ssh.exe', 'nas' }, },
--- }
+}
 
 
 return c
