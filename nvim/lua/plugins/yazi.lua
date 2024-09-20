572439ea3bce95d4d3fbfdf698ddc7d80b4cb750
Lecter <uxfion@gmail.com>
2024-09-20 16:05:28 +0800

    yazi.nvim and oh-my-tmux update

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -1,30 +1,34 @@
 ---@type LazySpec
 return {
   "mikavilpas/yazi.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
   event = "VeryLazy",
   keys = {
     -- 👇 in this section, choose your own keymappings!
     {
       "<leader>e",
-      function()
-        require("yazi").yazi()
-      end,
-      desc = "Open the file manager",
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
     },
     {
       -- Open in the current working directory
       "<leader>cw",
-      function()
-        require("yazi").yazi(nil, vim.fn.getcwd())
-      end,
+      "<cmd>Yazi cwd<cr>",
       desc = "Open the file manager in nvim's working directory" ,
+    },
+    {
+      -- NOTE: this requires a version of yazi that includes
+      -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+      '<c-up>',
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
     },
   },
   ---@type YaziConfig
   opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
     open_for_directories = false,
+    keymaps = {
+      show_help = '<f1>',
+    },
   },
 }
