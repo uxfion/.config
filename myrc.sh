335133993e96c0258ac51ce104143be21d92c172
Lecter <uxfion@gmail.com>
2024-10-17 15:17:03 +0800

    show_color (true color)

--- myrc.sh
+++ myrc.sh
@@ -98,6 +98,22 @@ show_size() {
     du -sh ./* ./.??* 2>/dev/null | sort -rh
 }
 
+show_color() {
+    awk 'BEGIN{
+        s="/\\/\\/\\/\\/\\"; s=s s s s s s s s;
+        for (colnum = 0; colnum<77; colnum++) {
+            r = 255-(colnum*255/76);
+            g = (colnum*510/76);
+            b = (colnum*255/76);
+            if (g>255) g = 510-g;
+            printf "\033[48;2;%d;%d;%dm", r,g,b;
+            printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+            printf "%s\033[0m", substr(s,colnum+1,1);
+        }
+        printf "\n";
+    }'
+}
+
 if [ -d "$HOME/.local/share/kitty-ssh-kitten/kitty/bin" ] && [[ ":$PATH:" != *":$HOME/.local/share/kitty-ssh-kitten/kitty/bin:"* ]]; then
     export PATH="$PATH:$HOME/.local/share/kitty-ssh-kitten/kitty/bin"
 fi
