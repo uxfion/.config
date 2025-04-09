7ee8d22f85c0854f20069ffadebdc1dbb3c6e870
Lecter <uxfion@gmail.com>
2025-04-09 23:20:35 +0800

    simplify wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,47 +5,29 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+config.exit_behavior = "Hold"
+config.window_close_confirmation = 'NeverPrompt'
+
+config.color_scheme = "Catppuccin Mocha"
 config.initial_cols = 100
 config.initial_rows = 30
-config.font_size = 12
-config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font = wezterm.font_with_fallback {
-    'JetBrainsMono Nerd Font',
-    'PingFang SC',  -- 🅒
-  }
+config.font_size = 13
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "Regular" })
 
-config.color_scheme = "Tokyo Night"
-config.window_background_opacity = 0.95
-
+config.use_fancy_tab_bar = false
 config.window_decorations = "RESIZE"
-config.use_fancy_tab_bar = true
-
-config.window_close_confirmation = 'NeverPrompt'
+config.window_background_opacity = 0.95
 
 
 config.default_prog = { 'pwsh' }
-config.default_cwd = "D:\\Lecter\\Downloads"
--- config.default_domain = 'SSH:hx90'
 
 config.launch_menu = {
     { label = 'PowerShell', args = { 'pwsh' }, },
     { label = 'Ubuntu', args = { 'ubuntu' }, },
-    { label = 'HX90', domain = { DomainName = 'SSH:hx90' }, },
 }
 
 
-local platform = require('platform')()
 local act = wezterm.action
-
-local mod = {}
-
-if platform.is_mac then
-   mod.SUPER = 'SUPER'
-   mod.SUPER_REV = 'SUPER|CTRL'
-elseif platform.is_win then
-   mod.SUPER = 'ALT' -- to not conflict with Windows key shortcuts
-   mod.SUPER_REV = 'ALT|CTRL'
-end
 
 config.keys = {
     { key = 'w', mods = 'CTRL', action = act.CloseCurrentTab({ confirm = false }) },
@@ -67,18 +49,8 @@ config.keys = {
     { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
 
     { key = 'F1', mods = 'NONE', action = act.ActivateCommandPalette },
-    { key = 'F2', mods = 'NONE', action = 'ActivateCopyMode' },
-    { key = 'F3', mods = 'NONE', action = act.ShowLauncher },
-    -- { key = 'F4', mods = 'NONE', action = act.ShowTabNavigator },
-    { key = 'F4', mods = 'NONE', action = act.SpawnCommandInNewTab({ args = { 'ubuntu' }, }) },
-
-    { key = 'F5', mods = 'NONE', action = act.SpawnCommandInNewTab({ domain = { DomainName = 'SSH:3090_1.lan' }, }) },
-    { key = 'F6', mods = 'NONE', action = act.SpawnCommandInNewTab({ domain = { DomainName = 'SSH:a40_1.lan' }, }) },
-    { key = 'F7', mods = 'NONE', action = act.SpawnCommandInNewTab({ domain = { DomainName = 'SSH:occc' },}) },
-    { key = 'F8', mods = 'NONE', action = act.SpawnCommandInNewTab({ domain = { DomainName = 'SSH:hx90' }, }) },
-
+    { key = 'F2', mods = 'NONE', action = act.ShowLauncher },
     { key = 'F11', mods = 'NONE', action = act.ToggleFullScreen },
-    { key = 'F12', mods = 'NONE', action = act.ShowDebugOverlay },
 }
 
 
